f41834263626fb7713d5e06115d8f086d6f64e60
Laurent Clavier <4008875+lrtcl@users.noreply.github.com>
2023-01-18 18:38:48 +0100

    clean Libtest files

--- readme.md
+++ readme.md
@@ -2,16 +2,15 @@
 
 1. create monorepo
 2. install React + Vite in the ```library```  package
-3. delete react vite example app
+3. delete React + Vite example app
 4. create a React + Vite test site in the ```libtest``` site
 5. Configure Vite to build a library instead of a site
 6. configure css modules for typescript
 7. Create first component and test it
-
+8. Configure vite autoprefixing
 
 ## TODO:
 * add postcss plugins
-* configure vite autoprefixing
 * add eslint, stylelint, prettier
 * add storybook
 * start coding!!!
--- sites/libtest/src/App.tsx
+++ sites/libtest/src/App.tsx
@@ -1,34 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import { Helloworld } from 'react-library-vite'
-import './App.css'
 import '../../../node_modules/react-library-vite/dist/index.css'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
       <Helloworld text="Yo Bitches!!!" color="accent" variant="strong" />
       <Helloworld text="lorem ipsum dolor sit amet." />
     </div>
