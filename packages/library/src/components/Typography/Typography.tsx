02e8969e648f06882a53169721507b9de4351804
Laurent Clavier <4008875+lrtcl@users.noreply.github.com>
2023-02-24 18:23:14 +0100

    WIP Typography

--- packages/library/src/components/Typography/Typography.tsx
+++ packages/library/src/components/Typography/Typography.tsx
@@ -1,3 +1,4 @@
+import classnames from "classnames";
 import React from "react";
 
 export interface TypographyProps extends React.HTMLAttributes<HTMLHeadingElement> {
@@ -5,13 +6,28 @@ export interface TypographyProps extends React.HTMLAttributes<HTMLHeadingElement
    * The tag used for the root node.
    * Either a string to use a HTML element or a component.
    */
-  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p",
-  children: React.ReactNode
+  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p",
+  variant?: "heading1" | "heading2" | "heading3" | "heading4" | "heading5" | "heading6" | "body",
+  children: React.ReactNode,
+  className?: string
 };
 
-export const Typography: React.FC<TypographyProps> = ({ tag = "p", children }: TypographyProps) => {
+export const Typography: React.FC<TypographyProps> = ({ tag = "p", variant = "body", children, className }: TypographyProps) => {
   const Tag = ({ ...props }: React.HTMLAttributes<HTMLHeadingElement>) =>
     React.createElement(tag, props, children)
+  let classNames = classnames(
+    "mylib--typography",
+    {
+      "mylib--typography_heading1": variant === "heading1",
+      "mylib--typography_heading2": variant === "heading2",
+      "mylib--typography_heading3": variant === "heading3",
+      "mylib--typography_heading4": variant === "heading4",
+      "mylib--typography_heading5": variant === "heading5",
+      "mylib--typography_heading6": variant === "heading6",
+      "mylib--typography_body": variant === "body",
+    },
+    className
+  )
 
-  return <Tag>{children}</Tag>
+  return <Tag className={classNames}>{children}</Tag>
 };
--- packages/library/src/stories/Typography.stories.ts
+++ packages/library/src/stories/Typography.stories.ts
@@ -12,7 +12,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    tag: 'h1',
     children: 'lorem ipsum dolor sit amet'
   }
 };
